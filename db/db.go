9efc6bf6d3acc37743f6f4e02a0d99b67575fd25
t-okuji <t.okuji34@gmail.com>
2024-08-18 21:38:10 +0900

    feat: change mariadb to mysql

--- db/db.go
+++ db/db.go
@@ -15,9 +15,13 @@ func NewDB() *gorm.DB {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", os.Getenv("MARIADB_USER"),
-		os.Getenv("MARIADB_PASSWORD"), os.Getenv("MARIADB_HOST"),
-		os.Getenv("MARIADB_PORT"), os.Getenv("MARIADB_DATABASE"))
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=Local",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DATABASE"),
+	)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalln(err)
