69c0a1b91683665f499c79f4768871735f8ddb4d
t-okuji <t.okuji34@gmail.com>
2024-08-20 23:32:10 +0900

    feat: Protect routes with JWT Token

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.23.0
 
 require (
 	github.com/alexedwards/argon2id v1.0.0
-	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/go-chi/chi/v5 v5.0.7
 	github.com/go-chi/cors v1.2.1
 	github.com/go-chi/jwtauth/v5 v5.3.1
 	github.com/joho/godotenv v1.5.1
--- router/router.go
+++ router/router.go
@@ -1,13 +1,18 @@
 package router
 
 import (
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
+	"github.com/go-chi/jwtauth/v5"
 	"github.com/t-okuji/go-chi-gorm-jwt/controller"
 )
 
-func NewRouter(uc controller.IUserController) *chi.Mux {
+func NewRouter(uc controller.IUserController) http.Handler {
+	tokenAuth := jwtauth.New("HS256", []byte(os.Getenv("TOKEN_SECRET")), nil)
 	r := chi.NewRouter()
 	r.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"https://*", "http://*"},
@@ -17,11 +22,29 @@ func NewRouter(uc controller.IUserController) *chi.Mux {
 		AllowCredentials: false,
 		MaxAge:           300,
 	}))
-	r.Use(middleware.Logger)
+	// r.Use(middleware.Logger)
 
-	r.Post("/signup", uc.SignUp)
-	r.Post("/login", uc.LogIn)
-	r.Post("/logout", uc.LogOut)
+	// Protected routes
+	r.Group(func(r chi.Router) {
+		// Seek, verify and validate JWT tokens
+		r.Use(jwtauth.Verifier(tokenAuth))
+
+		// Handle valid / invalid tokens.
+		r.Use(jwtauth.Authenticator(tokenAuth))
+
+		r.Get("/admin", func(w http.ResponseWriter, r *http.Request) {
+			_, claims, _ := jwtauth.FromContext(r.Context())
+			w.Write([]byte(fmt.Sprintf("user_id : %v", claims["user_id"])))
+		})
+
+		r.Post("/logout", uc.LogOut)
+	})
+
+	// Public routes
+	r.Group(func(r chi.Router) {
+		r.Post("/signup", uc.SignUp)
+		r.Post("/login", uc.LogIn)
+	})
 
 	return r
 }
